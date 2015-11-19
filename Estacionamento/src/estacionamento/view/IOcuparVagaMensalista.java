45fd4ea96eeb70cd475f7ca39e334de0fc6d422e
Flávio Keglevich <fkeglevich@gmail.com>
2015-11-19 12:35:02 -0200

    Criei a interface IOcuparVagaMensalista para alguém (Gustavo) implementar ela :D

---
+++ Estacionamento/src/estacionamento/view/IOcuparVagaMensalista.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estacionamento.view;
+
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author flavio
+ */
+public interface IOcuparVagaMensalista {
+    
+    String getCpf();
+    
+    void addOuvinteOk(ActionListener ouvinte);
+    
+}
